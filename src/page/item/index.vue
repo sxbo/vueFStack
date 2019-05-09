bd08dd74457b2dd96409a00acb7bf72266419951
xiaobo <henaxiaobo168@163.com>
2019-05-09 21:45:23 +0800

    ui

--- src/page/item/index.vue
+++ src/page/item/index.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
-        item
+        <itemcontainer father-component="item"></itemcontainer>
     </div>
 </template>
+
+<script>
+import itemcontainer from '../../components/itemcontainer'
+export default {
+    name: 'item',
+    components: {
+        itemcontainer        
+    },
+
+    created() {
+        this.$store.commit('REMBER_TIME');
+    }
+}
+</script>
